a2626f20297f1b280a7c5bafbfdb9e0e0ec8f154
GALGALLOR <69774313+GALGALLOR@users.noreply.github.com>
2020-12-01 00:58:32 +0300

    Update trivia.py
    Addition of LeaderBoard and some fixing of bugs occurred here

--- trivia.py
+++ trivia.py
@@ -1,48 +1,143 @@
 from flask import Flask,request,render_template,redirect,flash,url_for,session
+from flask_mysqldb import MySQL
 app=Flask(__name__)
 app.secret_key='mimi'
+marks=0
+mydb = MySQL(app)
 
+app.config['MYSQL_HOST']='localhost'
+app.config['MYSQL_USER']='root'
+app.config['MYSQL_PASSWORD']='GALGALLO10'
+app.config['MYSQL_DB']='Trivia'
 
 
 @app.route('/')
 def base():
 	return redirect(url_for('home'))
+
 @app.route('/home',methods=['GET','POST'])
 def home():
 	if request.method =='POST':
 		names=request.form['names']
 		session['names']=names
-		return redirect(url_for('questions'))
+		return redirect(url_for('question1'))
 	else:
-			return render_template('trivia.html')
+		session.pop('names',None)
+		return render_template('trivia.html')
 
-@app.route('/Questions',methods=['GET','POST'])
-def questions():
+@app.route('/Question1',methods=['GET','POST'])
+def question1():
 	if 'names' in session:
 		if request.method =='POST':
 			try:
 				president=request.form['president']
-				ww1=request.form['ww1']
-				print(ww1)
-				print(president)
-			finally:
-				if president=='kenyatta':
-					p='correct! President is Kenyatta'
-				elif president !='kenyatta':
-					p='Wrong!President is kenyatta'
-				if ww1 == '1918':
-					ww='yes, World War 1 occured in 1918'
-				elif ww1 !='1918':
-					ww = 'No , World War one occured in 1918'
-				return f'<h1>{p},{ww}  </h1>'
+				if president == 'kenyatta':
+					global marks
+					marks=marks+1
+					print(marks)
+					return redirect(url_for('question2'))
+				else:
+				
+					return redirect(url_for('question2'))
+			except:
+				return redirect(url_for('question2'))
 		else:
-			return render_template('questions.html')
+			return render_template('question1.html')
 	else:
-		return render_template('trivia.html')
+		return redirect(url_for('home'))
+
+@app.route('/Question2',methods=['GET','POST'])
+def question2():
+	if 'names' in session:
+		if request.method =='POST':
+			try:
+				ww1=request.form['ww1']
+				if ww1 == '1918':
+					global marks
+					marks=marks+1
+					print(marks)
+					return redirect(url_for('question3'))
+				else:
+					return redirect(url_for('question3'))
+			except:
+				return redirect(url_for('question3'))
+
+		else:
+			return render_template('question2.html')
+	else:
+		return redirect(url_for('home'))
+
+
+@app.route('/Question3',methods=['GET','POST'])
+def question3():
+	if 'names' in session:
+		if request.method =='POST':
+			try:
+				ageobama=request.form['ageobama']
+				if ageobama == '59':
+					global marks
+					marks=marks+1
+					print(marks)
+					return redirect(url_for('question4'))
+				else:
+					return redirect(url_for('question4'))
+			except:
+				return redirect(url_for('question4'))
+
+		else:
+			return render_template('question3.html')
+	else:
+		return redirect(url_for('home'))
+
+@app.route('/Question4',methods=['GET','POST'])
+def question4():
+	if 'names' in session:
+		if request.method =='POST':
+			try:
+				nile=request.form['nile']
+				if nile == '11':
+					global marks
+					marks=marks+1
+					print(marks)
+					
+					return redirect(url_for('done'))
+	
+				else:
+					
+					return redirect(url_for('done'))
+			except:
+				
+				return redirect(url_for('done'))
+
+		else:
+			return render_template('question4.html')
+	else:
+		return redirect(url_for('home'))
+
+@app.route('/Done',methods=['POST','GET'])
+def done():
+	if request.method=='POST':
+		global marks
+		marks=0
+		return redirect(url_for('home'))
+	else:
+		try:
+			names=session['names']
+			#insert data into database
+			cursor = mydb.connection.cursor()
+			cursor.execute("INSERT INTO UserData (names,score)VALUES(%s,%s)", (names, marks))
+			mydb.connection.commit()
+			#reveal the data into a leaderBoard
+			cursor.execute('SELECT names,score FROM UserData ORDER BY score DESC ')
+			names=cursor.fetchall()
+			return render_template('Answersheet.html',names=names,marks=marks)
+		except:
+			return redirect(url_for('home'))
+
 
 @app.route('/logout')
 def logout():
-	session.pop('username',None)
+	session.pop('names',None)
 	flash('logged out successfully')
 	return redirect(url_for('home'))
 
